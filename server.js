cafe4bb434fe8948de75108eb0f3c12d53588429
yaxiannnn <143898628+Hyzas466@users.noreply.github.com>
2026-05-07 21:42:16 +0700

    Change server port to 3000 and simplify CORS
    Updated the server port to 3000 and simplified CORS middleware.

--- server.js
+++ server.js
@@ -7,14 +7,10 @@ const notesRoutes = require('./routes/notesRoutes');
 require('./schema/Note'); // Import the schema for Sequelize sync
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors({
-    origin: ['http://localhost', 'http://127.0.0.1:5500', 'http://localhost:5000'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
-}));
+app.use(cors({}));
 app.use(express.json()); // Agar bisa menerima format JSON dari body request
 
 // Routes
